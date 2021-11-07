8922b83b418f28934ba740a288835c6aa80428ce
Jhonatan Alves <jhon_fla@hotmail.com>
2021-11-07 00:56:01 -0300

    Adding rounded rectangle

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-                .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 40)
+                .stroke(lineWidth: 5)
+            Text("Hello, world!")
+                .foregroundColor(.purple)
+        }
+        .foregroundColor(.blue)
+        .padding()
     }
 }
 
