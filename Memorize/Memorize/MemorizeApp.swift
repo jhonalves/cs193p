80e8ab24916d417f031f8a85825109b5cd77213c
Jhonatan Alves <jhon_fla@hotmail.com>
2021-11-07 00:54:34 -0300

    Creating xcode project - lecture 1

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jhonatan Alves on 05/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
