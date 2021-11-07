c7a1dd8cf6a1f612d2e11fefa36eeeda84fd45df
Jhonatan Alves <jhon_fla@hotmail.com>
2021-11-07 01:21:51 -0300

    Creating CardView struct - lecture 2

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,19 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 40)
-                .stroke(lineWidth: 5)
-            Text("Hello, world!")
-                .foregroundColor(.purple)
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
         .foregroundColor(.blue)
         .padding()
     }
 }
 
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill()
+                .foregroundColor(.white)
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("⚽️")
+                .font(.largeTitle)
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
