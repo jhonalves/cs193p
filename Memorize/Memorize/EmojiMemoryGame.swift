9f0df7567aa17c0e55360d8b2075aadbb0db71db
Jhonatan Alves <jhon_fla@hotmail.com>
2021-11-08 02:34:17 -0300

    Creating function to provide cards content

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 class EmojiMemoryGame {
-    private var model: MemoryGame<String> =
-        MemoryGame<String>(numberOfPairsOfCards: 4) { _ in "card content" }
+    static let emojis = ["⚽️", "🏀", "🏈", "⚾️", "🥎", "🎾", "🏐", "🏉", "🥏", "🎱", "🪀", "🏓", "🏸", "🏒", "🏑", "🥍", "🏏", "🪃", "🥅", "⛳️", "🪁", "🏹", "🎣", "🤿"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    private var model: MemoryGame<String> = createMemoryGame()
     
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards
