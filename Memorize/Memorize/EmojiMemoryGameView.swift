0b5e15993ac1f2a1bbc4db5d2b10008c61e40964
Jhonatan Alves <jhon_fla@hotmail.com>
2021-12-21 20:40:07 -0300

    Implementing ViewBuilder on cards showing

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -11,20 +11,17 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var game: EmojiMemoryGame 
     
     var body: some View {
-//        ScrollView {
-//            LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
-//                ForEach(game.cards) { card in
-        AspectVGrid(items: game.cards, aspectRatio: 2/3, content: { card in
-            CardView(card: card)
-                .padding(4)
-                .onTapGesture {
-                    game.choose(card)
-                }
-        })
-            
-//                }
-//            }
-//        }
+        AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
+            if card.isMatched && !card.isFaceUp {
+                Rectangle().opacity(0)
+            } else {
+                CardView(card: card)
+                    .padding(4)
+                    .onTapGesture {
+                        game.choose(card)
+                    }
+            }
+        }
         .foregroundColor(.red)
         .padding()
     }
